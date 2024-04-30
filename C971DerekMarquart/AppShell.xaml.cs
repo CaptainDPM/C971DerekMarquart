a26c0216ad8d16bee563aec5ea799521c4be677b
DPAA <DPAA@DESKTOP-3BCS7FA>
2024-04-30 08:29:09 -1000

    Add project files.

---
+++ C971DerekMarquart/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace C971DerekMarquart
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ C971DerekMarquart/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography;
+
+namespace C971DerekMarquart
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void OnCounterClickedRandom(object sender, EventArgs e)
+        {
+            Random randomNum = new Random();
+            CounterBtn2.Text = randomNum.Next().ToString();
+        }
+    }
+
+}
